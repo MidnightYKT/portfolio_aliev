b394c7bae510d355e0500ccca18631090108f3b0
MidnightYKT <nomiama_ykt@mail.ru>
2024-04-08 05:24:21 -0700

    update

--- src/data/index.js
+++ src/data/index.js
@@ -59,7 +59,7 @@ export const data = [
     },
     {
         id: 2,
-        title: 'Kanban React',
+        title: 'KANBAN REACT',
         description:
             'The Kanban Board React.js application is a dynamic project designed to streamline task management and enhance productivity. Developed with the modern web technology of React.js, this application serves as a visual project management tool.',
         image: PorjectKanban,
