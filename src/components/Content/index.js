673505bdebf9f789138d1d60afd3b239441a681d
MidnightYKT <nomiama_ykt@mail.ru>
2024-04-13 14:34:31 -0700

    added dark mode for component Content

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -30,7 +30,7 @@ const img = [
 const Content = () => {
     return (
         <div className="w-full text-center" id="content">
-            <div className="mx-auto max-w-[1000px] text-[#2d2e32]">
+            <div className="mx-auto max-w-[1000px] text-[#2d2e32] dark:text-white">
                 <div className="mx-4 mb-4 p-2 lg:mx-0 lg:p-0">
                     <div className="flex flex-col-reverse md:flex-row">
                         <div className="lg:basis-[60%]">
@@ -44,7 +44,7 @@ const Content = () => {
                                             className="w-7 h-7 lg:w-12 lg:h-12 inline lg:ml-3 lg:mb-2 duration-500 hover:scale-125"
                                         />
                                     </p>
-                                    <div className="font-normal text-gray-600 text-lg lg:mt-6 lg: mb-2">
+                                    <div className="font-normal text-gray-600 dark:text-zinc-400 text-lg lg:mt-6 lg: mb-2">
                                         Hi, I'm Bek Aliev. A dedicated Front-end React Developer
                                         based in Seattle, WA
                                     </div>
@@ -93,7 +93,7 @@ const Content = () => {
                     </div>
                     <div className="md:flex mt-10">
                         <div className="font-bold text-base py-4 pr-3 mb-3 md:mb-0">Tech Stack</div>
-                        <div className="min-h-[1em] w-px bg-[#2d2e32] mx-2 hidden md:block"></div>
+                        <div className="min-h-[1em] w-px bg-[#2d2e32] dark:bg-zinc-400 mx-2 hidden md:block"></div>
                         <ul className="grid grid-cols-4 md:flex flex-wrap gap-8 md:gap-4 py-2 md:grid-cols-none">
                             {img.map((item, i) => (
                                 <li
